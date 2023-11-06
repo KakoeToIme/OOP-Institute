9827065259a17c9ec8c0a6a61a534a0c243afa32
KakoeToIme <146635548+KakoeToIme@users.noreply.github.com>
2023-11-07 01:10:28 +0600

    Perimetr.java

--- Лаба1/Код программы/Perimetr.java
+++ Лаба1/Код программы/Perimetr.java
@@ -1,12 +1,14 @@
 package laba1n11;
 
+import java.util.ArrayList;
+
 public class Perimetr {
 	double Per = 0;
-	 public double SearchPer(double[] coord, int N){
-	        for (int i = 0; i < 2*N; i+=2){
-	            for (int j = 0; j < 2*N; j+=2){
-	                for (int k = 0; k < 2*N; k+=2){
-	                    double d = Math.sqrt(Math.pow(coord[i] - coord[j], 2) + Math.pow(coord[i+1] - coord[j+1], 2)) + Math.sqrt(Math.pow(coord[i] - coord[k], 2) + Math.pow(coord[i+1] - coord[k+1], 2)) + Math.sqrt(Math.pow(coord[j] - coord[k], 2) + Math.pow(coord[j+1] - coord[k+1], 2));
+	 public double SearchPer(ArrayList<Double> xcoord, ArrayList<Double> ycoord,  int N){
+	        for (int i = 0; i < N; i++){
+	            for (int j = 0; j < N; j++){
+	                for (int k = 0; k < N; k++){
+	                    double d = Math.sqrt(Math.pow(xcoord.get(i) - xcoord.get(j), 2) + Math.pow(ycoord.get(i) - ycoord.get(j), 2)) + Math.sqrt(Math.pow(xcoord.get(i) - xcoord.get(k), 2) + Math.pow(ycoord.get(i) - ycoord.get(k), 2)) + Math.sqrt(Math.pow(xcoord.get(j) - xcoord.get(k), 2) + Math.pow(ycoord.get(j) - ycoord.get(k), 2));
 	                    if (Per < d) Per = d;
 	                }
 	            }

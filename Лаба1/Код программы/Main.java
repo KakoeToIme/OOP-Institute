f057746ec9cc2209c6a3ff898cdc1204eaace384
KakoeToIme <146635548+KakoeToIme@users.noreply.github.com>
2023-11-07 01:09:53 +0600

    Main.java

--- Лаба1/Код программы/Main.java
+++ Лаба1/Код программы/Main.java
@@ -1,31 +1,35 @@
 package laba1n11;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+
 public class Main {
 	public int N = 0;
-    public double[] coord = new double[2*N];
+    public ArrayList<Double> xcoord = new ArrayList<Double>(N);
+    public ArrayList<Double> ycoord = new ArrayList<Double>(N);
     public static void main(String[] args) {
     Main main = new Main();
     Scanner sc1 = new Scanner(System.in);
     while(true) {
-    System.out.println("Введите количство точек");
-    try {
-    main.N = sc1.nextInt();
-	main.coord = new double[2*main.N];
-    break;
-    } catch(InputMismatchException e) {
+    	System.out.println("Введите количство точек");
+    	try {
+    			main.N = sc1.nextInt();
+    			break;
+    		} catch(InputMismatchException e) {
     	System.out.println("Error");
-    }
+    		}
     }
     System.out.println("Введите координаты точек");
     Scanner sc2 = new Scanner(System.in);
-    for (int i = 0; i < 2* main.N; i++){
-        main.coord[i] = sc2.nextDouble();
+    for (int i = 0; i < main.N; i++){
+        main.xcoord.add(sc2.nextDouble());
+        main.ycoord.add(sc2.nextDouble());
     }
     Perimetr perim = new Perimetr();
-    System.out.println(perim.SearchPer(main.coord, main.N));
+    System.out.println(perim.SearchPer(main.xcoord, main.ycoord, main.N));
     sc1.close();
     sc2.close();
     }
 }
+
